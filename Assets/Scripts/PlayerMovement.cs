4516788a6890f1f5e697eaca8c10109288d0db36
Qutbuddin Johar <qutubjohar@gmail.com>
2025-11-28 12:20:26 +0530

    Movement ,input and Camera
    Added move inputs, Setup a mockup camera and added movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] Rigidbody rb;
+    Vector2 moveInput;
+   
+   
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+       
+        
+    }
+
+    private void FixedUpdate()
+    {
+        move();
+    }
+    void move()
+    {
+        rb.linearVelocity = 3f * new Vector3(moveInput.x, rb.linearVelocity.y, moveInput.y);
+    }
+
+   
+}
